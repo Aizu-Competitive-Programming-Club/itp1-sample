0fbcdc3a5ba66820dc6e28808244b68f11adc99e
Luzhiled <Luzhiled+github@gmail.com>
2023-01-23 21:18:34 +0900

    update for simplicity

--- 3-b/main.cpp
+++ 3-b/main.cpp
@@ -4,7 +4,11 @@ int main() {
   int x;
   int case_count = 0;
 
-  while (std::cin >> x, x) {
+  while (std::cin >> x) {
+    if (x == 0) {
+      break;
+    }
+
     case_count++;
 
     std::cout << "Case " << case_count << ": " << x << std::endl;
