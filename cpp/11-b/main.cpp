baf76763e63d709410584c0d3bf08cd921247a7b
Luzhiled <Luzhiled+github@gmail.com>
2023-01-24 00:46:03 +0900

    implement 11-c

---
+++ cpp/11-b/main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+class Dice {
+  std::vector< int > as;
+
+  void rotate_inner(std::vector< int > is) {
+    for (int i = 1; i < (int)is.size(); i++) {
+      std::swap(as[is[i - 1]], as[is[i]]);
+    }
+  }
+
+ public:
+  Dice() {}
+  Dice(std::vector< int > as_) : as(as_) {}
+
+  int top() {
+    return as[0];
+  }
+
+  int front() {
+    return as[1];
+  }
+
+  int right() {
+    return as[2];
+  }
+
+  void rotate(char c) {
+    if (c == 'N') {
+      rotate_inner({0, 1, 5, 4});
+    }
+    if (c == 'S') {
+      rotate_inner({0, 4, 5, 1});
+    }
+    if (c == 'E') {
+      rotate_inner({0, 3, 5, 2});
+    }
+    if (c == 'W') {
+      rotate_inner({0, 2, 5, 3});
+    }
+    if (c == 'R') {
+      rotate_inner({1, 3, 4, 2});
+    }
+    if (c == 'L') {
+      rotate_inner({1, 2, 4, 3});
+    }
+  }
+
+  void normalize(int Top, int Front) {
+    for (int i = 0; i < 6; i++) {
+      if (top() == Top) {
+        break;
+      }
+
+      if (i % 2 == 0) rotate('N');
+      else rotate('E');
+    }
+
+    while (front() != Front) {
+      rotate('R');
+    }
+  }
+};
+
+int main() {
+  std::vector< int > as(6);
+  for (int i = 0; i < (int)as.size(); i++) {
+    std::cin >> as[i];
+  }
+
+  Dice dice(as);
+
+  int q;
+  std::cin >> q;
+
+  for (int i = 0; i < q; i++) {
+    int top, front;
+    std::cin >> top >> front;
+
+    dice.normalize(top, front);
+    std::cout << dice.right() << std::endl;
+  }
+}
