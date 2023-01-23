0997c6df36b77da2f66cd1db629ce56bc79e4de3
Luzhiled <Luzhiled+github@gmail.com>
2023-01-23 21:55:08 +0900

    implement 5-c

---
+++ 5-c/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+  int h, w;
+
+  while (std::cin >> h >> w) {
+    if (h == 0 and w == 0) {
+      break;
+    }
+
+    for (int i = 0; i < h; i++) {
+      for (int j = 0; j < w; j++) {
+        if ((i + j) % 2 == 0) {
+          std::cout << '#';
+        } else {
+          std::cout << '.';
+        }
+      }
+      std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+  }
+}
